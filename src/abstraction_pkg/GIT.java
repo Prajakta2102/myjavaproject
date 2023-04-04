dc907d9d6275bbdec51f3ed7e328016de5a0617a
Prajakta2102 <prajaktasagane94@gmail.com>
2023-04-04 09:35:03 +0530

    Add operarion is added to GIT class

--- src/abstraction_pkg/GIT.java
+++ src/abstraction_pkg/GIT.java
@@ -6,6 +6,11 @@ public class GIT {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome,GIT...!!!");
 		System.out.println("Successfully done GIT...!!!");
+		int a=20;
+		int b=30;
+		int add=a+b;
+		System.out.println(add);
+
 
 	}
 
